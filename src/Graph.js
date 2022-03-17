fa0857097072ddf4eaf942e15f1e76c22214a436
Yavor Tenev <trustos@gmail.com>
2022-03-17 08:11:30 +0200

    Ongoing positioning.

--- src/Graph.js
+++ src/Graph.js
@@ -8,7 +8,50 @@ import * as tiny from './mock_data/tiny_edited.json';
 
 function MyCustomGraph () {
 	const sigma = useSigma();
+	const isStartNode = (node, connectors) => connectors.filter(connector => connector.target === node.id).length === 0;
+	const getNodeById = (nodes, nodeId) => nodes.filter(node => node.id === nodeId);
+	// const getChildren = () => 
+
+	const addNodePositions = (nodes, connectors) => {
+		let parsedNodes = nodes.map(node => node);
+		const terminate = { type: 'terminate', name: 'TERMINATE', id: 'terminate', isValid: true, color: '#FFA500', x: 0, y: 300 };
+
+		parsedNodes.unshift(terminate);
+
+		console.log(parsedNodes);
+
+		parsedNodes.forEach(node => {
+
+			//Check if is starting node
+			if (isStartNode(node, connectors)) {
+				node.x = 0;
+				node.y = 0;
+			} else {
+				const pointingLink = connectors.filter(connector => connector.target === node.id)[0];
+				// console.log(pointingLink);
+				const parent = getNodeById(parsedNodes, pointingLink.source);
+				
+				const parentX = parent[0].hasOwnProperty('x') ? parent[0].x : 1;
+				const parentY = parent[0].hasOwnProperty('y') ? parent[0].y : 1;
+
+				console.log(node.id);
+
+				if (node.id.includes('_entrypoint') ) {
+					node.x = parentX + 50;
+				} else {
+					node.x = node.x !== undefined ? node.x : parentX + 5;
+				}
+				node.y = node.y !== undefined ? node.y : (node.y % 2 ? parentY - 15 : parentY + 15);
+
+				console.log(node);
+			}
+		});
+
+		return parsedNodes;
+	};
+
 	const loadGraph = useLoadGraph();
+
 
 	// const settings = {multi: true};
 	// const graph = new MultiGraph();
@@ -32,31 +75,50 @@ function MyCustomGraph () {
 		const canvas = Array.from(tiny);
 		const parser = new Parser(canvas);
 		const {nodes, connectors} = parser.parse();
+		// console.log(addNodePositions(nodes));
+		const parsedNodes = addNodePositions(nodes, connectors);
 		
 		//If contains modulename_EntryPoint_something - start new circle based on current modules coordinates
 		//Until id contains modulename_ExitPoint_something
 
-		graph.addNode('terminate', {
-			label: 'TERMINATE',
-			x: 0, 
-			y: 0,
-			color: '#eb2e63',
-			size: 30
-		})
-		nodes.forEach((node, idx) => {
-			const x = 10 * Math.cos(Math.PI * 2 * idx / nodes.length );
-			const y = 10 * Math.sin(Math.PI * 2 * idx / nodes.length );
+		// graph.addNode('terminate', {
+		// 	label: 'TERMINATE',
+		// 	x: 20, 
+		// 	y: 20,
+		// 	color: '#eb2e63',
+		// 	size: 30
+		// });
+
+		// const getParent = () => {};
+
+		const numberOfConnections = nodeId => connectors.filter(connector => connector.target === nodeId).length;
+
+		console.log(parsedNodes);
+		
+		parsedNodes.forEach((node, idx) => {
+			// const x = node.x || (10 * Math.cos(Math.PI * 2 * idx / nodes.length ));
+			// const y = node.y || (10 * Math.sin(Math.PI * 2 * idx / nodes.length ));
+
+			const x = node.x;
+			const y = node.y;
+			// const x = node.x !== undefined ? node.x : 1;
+			// const y = node.y !== undefined ? node.x : 1;
+
+			const color = node => {
+				console.log(node.color || node.isValid ? '#00FF00' : '#FF0000');
+				return node.color || (node.isValid ? '#00FF00' : '#FF0000');
+			};
 
 			graph.addNode(node.id, 
 				{
 					label: node.name,
 					x, 
 					y,
-					color: node.isValid ? '#00FF00' : '#FF0000',
-					size: 20
+					color: color(node),
+					size: 10 * (numberOfConnections(node.id) || 1)
 				}
 			);
-		});
+		}, []);
 
 		// graph('terminate', {})
 
@@ -75,7 +137,7 @@ function MyCustomGraph () {
 		// graph.addNode("Truman", { label: "Truman", x: 0, y: 0, color: "#00F", size: 50 });
 		// graph.addNode("Washington", { label: "Washington", x: 2, y: 2, color: "#00F", size: 50 });
 		// graph.addEdge("Jessica", "Truman", { color: "#0f0f0f", size: 25, label: 'Connection', type: 'arrow' });
-	}, []);
+	});
 
 	// return <></>;
 	return <></>;
